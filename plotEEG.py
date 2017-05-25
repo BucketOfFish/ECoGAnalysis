392145a44d15130f1023dd65aa7fd7dd20ce729a
Matt Zhang <set.stun@gmail.com>
2017-05-25 07:12:38 -0500

    Added average plots and figure saving

--- plotEEG.py
+++ plotEEG.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import h5py
 import numpy as np
+import os
 
 # load data
-data = h5py.File("EC2_blocks_1_8_9_15_76_89_105_CV_HG_align_window_-0.5_to_0.79_file_nobaseline.h5")
+data = h5py.File("Data/EC2_blocks_1_8_9_15_76_89_105_CV_HG_align_window_-0.5_to_0.79_file_nobaseline.h5")
 X_data = data['Xhigh gamma'][:]
 y_data = data['y'][:] # phoneme label
 
@@ -16,6 +17,7 @@ sort_order = y_data.argsort() # sort by phonemes
 X_data = X_data[sort_order]
 y_data = y_data[sort_order]
 
+# plot individual events
 def plotEvent(eventN):
     X = X_data[eventN]
     y = y_data[eventN]
@@ -24,8 +26,22 @@ def plotEvent(eventN):
     plt.title("Event " + str(eventN) + " - EEG signal during production of phoneme " + str(y))
     plt.xlabel("Time step")
     plt.ylabel("ECOG channel")
-    plt.show()
+    directory = "Plots/Phoneme" + str(y) + "/"
+    if not os.path.exists(directory): os.makedirs(directory)
+    plt.savefig(directory + "Event" + str(eventN) + ".pdf", bbox_inches="tight")
 
-
-for eventN in range(1000):
+for eventN in range(len(y_data)):
     plotEvent(eventN)
+
+# plot averages for each phoneme
+for phoneme in range(max(y_data)):
+    indices = [y_data == phoneme]
+    X = np.mean(X_data[indices], axis=0)
+
+    plt.imshow(X, cmap=cm.gist_rainbow, vmin=0, vmax=1)
+    plt.title("Average EEG signal during production of phoneme " + str(phoneme))
+    plt.xlabel("Time step")
+    plt.ylabel("ECoG channel")
+    directory = "Plots/Averages/"
+    if not os.path.exists(directory): os.makedirs(directory)
+    plt.savefig(directory + "Phoneme" + str(phoneme) + ".pdf", bbox_inches="tight")
