0679f0bad807ccabe3b4a6627d8e82321f971653
Matt Zhang <set.stun@gmail.com>
2018-02-11 22:09:04 -0600

    Made augmentation code read settings in a module way that preserves history

--- DataAugmentation/augment.py
+++ DataAugmentation/augment.py
@@ -8,19 +8,7 @@ import sys, os
 # OPTIONS #
 ###########
 
-original_filename = "../../Data/ECoG/Original/EC2_blocks_1_8_9_15_76_89_105_CV_HG_align_window_-0.5_to_0.79_file_nobaseline.h5"
-new_filename = "../../Data/ECoG/001.h5"
-overwrite = False # whether it's ok to overwrite an existing output file
-n_isolated_samples = 5 # samples to keep separate and not use in augmentation (will later be used as test samples)
-total_samples_per_class = 500 # total number of samples we want to end up with for each CV pair
-interpolation = True
-gaussian_noise = True
-gaussian_noise_sigma = 0.5
-time_shifting = True
-max_steps_timeshift = 10
-amplitude_scaling = True
-min_amplitude_scale = 0.5
-max_amplitude_scale = 2
+from Settings.settings_002 import *
 
 ########################
 # AUGMENTATION METHODS #
